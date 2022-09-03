8b3822c90b452f9284b86f6fa11d623826f34a72
Krithik22 <krithiksaiprasad22@gmail.com>
2022-09-03 19:34:11 +0530

    added hero image on search

--- SuperHero App/script.js
+++ SuperHero App/script.js
@@ -2,14 +2,20 @@ const apiKey = '1722712481423675'
 const searchBar = document.getElementById('searchBar')
 const searchButton = document.getElementById('searchButton')
 
-const getHero = (name) => {
-    fetch(`https://superheroapi.com/api.php/1722712481423675/245`)
+const heroImage = document.getElementById('heroImage')
+
+const getHero = (heroName) => {
+    fetch(`https://www.superheroapi.com/api.php/${apiKey}/search/${heroName}`)
     .then(response => response.json())
-    .then(json => console.log(json))
+    .then(json => {
+        console.log(json)
+        console.log(json.results[0].image.url)
+        heroImage.setAttribute('src',json.results[0].image.url)
+    })
 }
 
 searchButton.onclick = () => {
     const heroName = searchBar.value
     getHero(heroName)
-    // searchBar.value = ''
+    searchBar.value = ''
 }
